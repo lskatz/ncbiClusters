6dd698b35b0fa41746c2a9dd3a5441755eeac532
Lee Katz - Aspen <gzu2@cdc.gov>
2016-07-12 11:06:24 -0400

    Added better color palette; moved more functions over to the lib

--- lib/perl5/NcbiClusters.pm
+++ lib/perl5/NcbiClusters.pm
@@ -2,21 +2,40 @@
 package NcbiClusters;
 use strict;
 use warnings;
+use Data::Dumper qw/Dumper/;
+# Alphabetize data dump keys
+$Data::Dumper::Sortkeys=sub{my($hash)=@_; return [sort {lc $a cmp lc $b} keys(%$hash)]};
 
 use Net::FTP;
+use Time::Piece;
 
 use Exporter qw/import/;
 
-our @EXPORT_OK = qw(logmsg listSets);
+our @EXPORT_OK = qw(logmsg listSets downloadAll parseDate findBiosample Dumper colorScheme);
 
 sub logmsg { print STDERR "$0: @_\n"; }
 
-=encoding UTF-8
 =head1 NcbiClusters
 
 A module for reporting on NCBI SNP clusters
 
+=head1 AUTHORS
+
+Lee Katz L<lkatz@cdc.gov>
+
+Errol Strain L<Errol.Strain@fda.hhs.gov>
+
 =head2 Methods
+
+=over 12
+
+=item C<Dumper>
+
+  Runs a Data Dump on a variable using L<Data::Dumper>
+
+=back 
+
+=cut
 
 =over 12
 
@@ -84,6 +103,240 @@ sub listRemoteDirs{
   $ftp->quit;
   
   return \@remoteDir;
+}
+
+=over 12
+
+=item C<downloadAll>
+
+  Downloads everything from the NCBI FTP site
+
+  Args:   $$settings{set}:     The taxon to look at. If set,
+                               the remote directories of this
+                               taxon will be reported instead.
+          $$settings{domain}:  The ncbi ftp domain
+          $$settings{tempdir}: Where things are being
+                               downloaded to
+          $$settings{outdir}:  Where things will eventually be
+                               moved to (but not by this sub)
+          $$settings{maxTrees} Maximum number of trees to
+                               download. Usually newer trees
+                               are downloaded first.
+
+
+=back
+
+=cut 
+
+sub downloadAll{
+  my($remoteDir,$settings)=@_;
+
+  my $ftp = Net::FTP->new($$settings{domain}, Debug => 0)
+    or die "Cannot connect to $$settings{domain}: $@";
+  $ftp->login("anonymous",'-anonymous@')
+      or die "Cannot login ", $ftp->message;
+  $ftp->ascii(); # download ascii encoding
+
+
+  #Retrieve the metadata file
+  logmsg "Retrieving metadata";
+  mkdir "$$settings{tempdir}/Metadata";
+  mkdir "$$settings{outdir}/Metadata";
+  $ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/Metadata/")
+    or die "Cannot change working directory. It is possible that '$$settings{set}' ($remoteDir) does not exist. ", $ftp->message;
+  my @metafiles = $ftp->ls("*.tsv");
+  foreach(@metafiles) {
+    $ftp->get($_,"$$settings{tempdir}/Metadata/$_")
+      or die "get failed", $ftp->message;
+  }
+
+  #Clusters directory files
+  mkdir "$$settings{tempdir}/Clusters";
+  mkdir "$$settings{outdir}/Clusters";
+  #logmsg "Retrieving SNP distances";
+  #$ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/Clusters/")
+  #  or die "Cannot change working directory ", $ftp->message;
+  #my @distfiles = $ftp->ls("*.SNP_distances.tsv");
+  #foreach(@distfiles) {
+  #  $ftp->get($_,"$$settings{tempdir}/Clusters/$_")
+  #    or die "get failed", $ftp->message;
+  #}
+  #
+  # Retrieve the list of newest isolates, also in this directory
+  logmsg "Retrieving a list of the latest isolates";
+  $ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/Clusters/")
+    or die "Cannot change working directory to Clusters. It is possible that '$$settings{set}' ($remoteDir) does not exist. ", $ftp->message;
+  my @newIsolateFiles=$ftp->ls("*.new_isolates.tsv")
+    or die "ERROR with command while in Clusters directory: ls *.new_isolates.tsv: ", $ftp->message;
+  foreach(@newIsolateFiles){
+    $ftp->get($_,"$$settings{tempdir}/Clusters/$_")
+      or die "get failed", $ftp->message;
+  }
+  
+
+  #Retrieve SNP trees
+  logmsg "Reading the remote SNP_trees directory";
+  mkdir "$$settings{tempdir}/SNP_trees";
+  mkdir "$$settings{outdir}/SNP_trees";
+  $ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/SNP_trees/")
+    or die "Cannot change working directory ", $ftp->message;
+  # Have to ls on the local folder because it would be too many results
+  # for the server to return.  Cannot ls on */*.newick.
+  my @SNP_dir = $ftp->ls("")
+    or die "ERROR: cannot get directory contents: ", $ftp->message;
+  die "INTERNAL ERROR: I only saw ".scalar(@SNP_dir) if(@SNP_dir < 1);
+  @SNP_dir=reverse(@SNP_dir);        # Reverse order so that we get mostly new trees first
+  @SNP_dir=grep(/^PDS\d+/,@SNP_dir); # Keep only directories that begin with PDS
+  # TODO filter trees to download, to save tons of time. Do the filtering
+  # step here instead of after all trees are downloaded.
+  logmsg scalar(@SNP_dir)." trees to download";
+  for(my $i=0;$i<@SNP_dir;$i++){
+    if($i % 100 == 0 && $i>0){
+      logmsg "Finished downloading $i trees out of ".scalar(@SNP_dir);
+    }
+
+    # If it is already here and downloaded, then don't
+    # download again.
+    my $localtree="$$settings{tempdir}/SNP_trees/$SNP_dir[$i].newick_tree.newick"; 
+    next if(-e $localtree);
+
+    # Look for the tree file
+    my @cluster_files=$ftp->ls($SNP_dir[$i]) or die "ERROR: could not read $SNP_dir[$i]: ",$ftp->message;
+    my $remotetree=(grep(/\.newick$/,@cluster_files))[0];
+    # there should be a newick tree in each dir, but don't bother downloading
+    # a null tree in case it happens.
+    next if(!$remotetree);
+
+    # Download the tree file.
+    $ftp->get($remotetree,$localtree)
+      or die "get failed", $ftp->message;
+
+    if($$settings{maxTrees} && $i >= $$settings{maxTrees}){
+      last;
+    }
+  }
+
+  $ftp->quit;
+
+  return scalar(@SNP_dir);
+}
+
+=over 12
+
+=item C<parseDate>
+  
+  Parses the date with C<Time::Piece>
+
+  Arguments:   $date      string in either MM/DD/YYYY format or YYYY-MM-DD format
+               $settings  (not used right now)
+
+=back
+
+=cut
+
+sub parseDate{
+  my($date,$settings)=@_;
+
+  return Time::Piece->strptime("12/31/1969","%m/%d/%Y") if($date=~/^\s*$/);
+
+  # Don't die just because Time::Piece craps out
+  my $timePiece;
+  eval{
+    if($date=~m|\d{1,2}/\d{1,2}/\d{2,4}|){
+      $timePiece=Time::Piece->strptime($date,"%m/%d/%Y");
+    } elsif($date=~m|\d{2,4}\-\d{1,2}\-\d{1,2}|){
+      $timePiece=Time::Piece->strptime($date,"%Y-%m-%d");
+    } else {
+
+      # IF MMYYDD formats don't work, try just the year
+      if($date=~/^\d{4}$/){
+        $timePiece=Time::Piece->strptime($date,"%Y");
+      } elsif($date=~/^(\d{4})/){ # just try first four digits of a number as a year
+        $timePiece=Time::Piece->strptime($1,"%Y");
+      }
+        
+    }
+    return $timePiece;
+  };
+  return $timePiece if(defined $timePiece);
+
+  logmsg "WARNING: I could not parse $date for dates. Using 12/31/1969.";
+  return Time::Piece->strptime("12/31/1969","%m/%d/%Y");
+}
+
+=over 12
+
+=item C<findBiosample>
+
+  Finds the biosample information for a given query
+
+  Args:  $biosample  string
+         $settings   hash     (not used right now)
+
+=back
+
+=cut
+
+sub findBiosample{
+  my($query,$settings)=@_;
+  
+  
+}
+
+=over 12
+
+=item C<colorScheme>
+
+  Defines a set of colors
+  Returns: array of RGB values, zero to one.
+           Each RGB value is a three-element list.
+  Palette was defined at http://paletton.com/#uid=62X0Z0k7xFM3Ur75UujeqG4j0KZ
+
+=back
+
+=cut
+
+sub colorScheme{
+  my($type,$settings)=@_;
+
+  $type||="rgb0";
+
+  # TODO allow for other types of color coding:
+  #   hex, rgb(0-255), rgba(0-255 + transparency), etc
+  # This currently only gives rgb0.
+  
+  my @color=(
+    [0,0,0],              # This will be the default color
+    [0.627,0.82,0.698],
+    [0.541,0.616,0.569],
+    [0.561,0.686,0.608],
+    [0.451,0.82,0.588],
+    [0.349,0.863,0.541],
+    [0.631,0.651,0.792],
+    [0.522,0.529,0.584],
+    [0.549,0.561,0.651],
+    [0.486,0.522,0.796],
+    [0.408,0.459,0.843],
+    [0.973,0.992,0.757],
+    [0.827,0.835,0.733],
+    [0.922,0.933,0.761],
+    [0.957,0.992,0.545],
+    [0.949,0.992,0.404],
+    [1,0.808,0.765],
+    [0.847,0.761,0.745],
+    [0.945,0.804,0.773],
+    [1,0.627,0.549],
+    [1,0.51,0.408],
+  );
+
+  # Interleave the different colors, but leave the zeroth
+  # color alone since it is for 'missing'.
+  # Also, leave the first color alone since we don't really
+  # need to shuffle it.
+  @color = sort { $$a[1] <=> $$b[1] } @color;
+
+  return @color if wantarray;
+  return \@color;
 }
 
 
--- scripts/downloadClusters.pl
+++ scripts/downloadClusters.pl
@@ -12,7 +12,6 @@ use Getopt::Long;
 use Net::FTP;
 use File::Basename qw/basename/;
 use File::Temp qw/tempdir/;
-use Data::Dumper;
 use POSIX qw/strftime/;
 use File::Copy qw/mv cp/;
 use List::Util qw/min max sum shuffle/;
@@ -27,7 +26,7 @@ use PostScript::Simple;
 use Time::Piece; # for parsing dates from Metadata.tsv
 use Config::Simple;
 use Algorithm::Combinatorics qw/variations_with_repetition/;
-use NcbiClusters qw/logmsg listSets/;
+use NcbiClusters qw/logmsg listSets downloadAll parseDate Dumper colorScheme/;
 
 local $0=basename($0);
 
@@ -98,117 +97,6 @@ sub main{
   makeReport($metadata,$PDG,$settings);
 
   return 0;
-}
-
-
-sub listRemoteDirs{
-  my($settings)=@_;
-  my $ftp = Net::FTP->new($$settings{domain}, Debug => 0)
-    or die "Cannot connect to $$settings{domain}: $@";
-  $ftp->login("anonymous",'-anonymous@')
-      or die "Cannot login ", $ftp->message;
-  
-  $ftp->cwd("//pathogen/Results/$$settings{set}")
-    or die "Cannot change working directory ", $ftp->message;
-
-  my @remoteDir=sort {$a cmp $b} $ftp->ls("");
-
-  $ftp->quit;
-  
-  return \@remoteDir;
-}
-
-sub downloadAll{
-  my($remoteDir,$settings)=@_;
-
-  my $ftp = Net::FTP->new($$settings{domain}, Debug => 0)
-    or die "Cannot connect to $$settings{domain}: $@";
-  $ftp->login("anonymous",'-anonymous@')
-      or die "Cannot login ", $ftp->message;
-  $ftp->ascii(); # download ascii encoding
-
-
-  #Retrieve the metadata file
-  logmsg "Retrieving metadata";
-  mkdir "$$settings{tempdir}/Metadata";
-  mkdir "$$settings{outdir}/Metadata";
-  $ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/Metadata/")
-    or die "Cannot change working directory. It is possible that '$$settings{set}' ($remoteDir) does not exist. ", $ftp->message;
-  my @metafiles = $ftp->ls("*.tsv");
-  foreach(@metafiles) {
-    $ftp->get($_,"$$settings{tempdir}/Metadata/$_")
-      or die "get failed", $ftp->message;
-  }
-
-  #Clusters directory files
-  mkdir "$$settings{tempdir}/Clusters";
-  mkdir "$$settings{outdir}/Clusters";
-  #logmsg "Retrieving SNP distances";
-  #$ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/Clusters/")
-  #  or die "Cannot change working directory ", $ftp->message;
-  #my @distfiles = $ftp->ls("*.SNP_distances.tsv");
-  #foreach(@distfiles) {
-  #  $ftp->get($_,"$$settings{tempdir}/Clusters/$_")
-  #    or die "get failed", $ftp->message;
-  #}
-  #
-  # Retrieve the list of newest isolates, also in this directory
-  logmsg "Retrieving a list of the latest isolates";
-  $ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/Clusters/")
-    or die "Cannot change working directory to Clusters. It is possible that '$$settings{set}' ($remoteDir) does not exist. ", $ftp->message;
-  my @newIsolateFiles=$ftp->ls("*.new_isolates.tsv")
-    or die "ERROR with command while in Clusters directory: ls *.new_isolates.tsv: ", $ftp->message;
-  foreach(@newIsolateFiles){
-    $ftp->get($_,"$$settings{tempdir}/Clusters/$_")
-      or die "get failed", $ftp->message;
-  }
-  
-
-  #Retrieve SNP trees
-  logmsg "Reading the remote SNP_trees directory";
-  mkdir "$$settings{tempdir}/SNP_trees";
-  mkdir "$$settings{outdir}/SNP_trees";
-  $ftp->cwd("//pathogen/Results/$$settings{set}/$remoteDir/SNP_trees/")
-    or die "Cannot change working directory ", $ftp->message;
-  # Have to ls on the local folder because it would be too many results
-  # for the server to return.  Cannot ls on */*.newick.
-  my @SNP_dir = $ftp->ls("")
-    or die "ERROR: cannot get directory contents: ", $ftp->message;
-  die "INTERNAL ERROR: I only saw ".scalar(@SNP_dir) if(@SNP_dir < 1);
-  @SNP_dir=reverse(@SNP_dir);        # Reverse order so that we get mostly new trees first
-  @SNP_dir=grep(/^PDS\d+/,@SNP_dir); # Keep only directories that begin with PDS
-  # TODO filter trees to download, to save tons of time. Do the filtering
-  # step here instead of after all trees are downloaded.
-  logmsg scalar(@SNP_dir)." trees to download";
-  for(my $i=0;$i<@SNP_dir;$i++){
-    if($i % 100 == 0 && $i>0){
-      logmsg "Finished downloading $i trees out of ".scalar(@SNP_dir);
-    }
-
-    # If it is already here and downloaded, then don't
-    # download again.
-    my $localtree="$$settings{tempdir}/SNP_trees/$SNP_dir[$i].newick_tree.newick"; 
-    next if(-e $localtree);
-
-    # Look for the tree file
-    my @cluster_files=$ftp->ls($SNP_dir[$i]) or die "ERROR: could not read $SNP_dir[$i]: ",$ftp->message;
-    my $remotetree=(grep(/\.newick$/,@cluster_files))[0];
-    # there should be a newick tree in each dir, but don't bother downloading
-    # a null tree in case it happens.
-    next if(!$remotetree);
-
-    # Download the tree file.
-    $ftp->get($remotetree,$localtree)
-      or die "get failed", $ftp->message;
-
-    if($$settings{maxTrees} && $i >= $$settings{maxTrees}){
-      last;
-    }
-  }
-
-  $ftp->quit;
-
-  return scalar(@SNP_dir);
 }
 
 sub readMetadata{
@@ -307,6 +195,10 @@ sub readMetadata{
         next;
       }
 
+      # Since there is a match on metadata from the line list,
+      # mark this isolate as having come from the line list.
+      $metadata{$index}{'is_in_lineList'}=1;
+
       # Add onto the large metadata hash.
       for my $pnHeader(@header){
         # If this header can be translated to an NCBI header,
@@ -324,11 +216,16 @@ sub readMetadata{
   # Get the rules for changing the label for each sample
   my $customLabel=new Config::Simple("config/headerMappings.ini")->get_block("custom_label");
   my @labelFields=split(/\s*\|\s*/,$$customLabel{label});
+  # For each isolate, modify its label
   for my $target_acc(keys(%metadata)){
     my $newLabel="$target_acc | ";
     for my $field(@labelFields){
-      next if(!defined($metadata{$target_acc}{$field}));
-      next if($metadata{$target_acc}{$field}=~/^NULL$|^\s*$|^missing$/);
+      if(
+           !defined($metadata{$target_acc}{$field})
+        || $metadata{$target_acc}{$field}=~/^NULL$|^\s*$|^missing$/i
+      ){
+        $metadata{$target_acc}{$field}="";
+      }
       $newLabel.=$metadata{$target_acc}{$field}.' | ';
     }
     $newLabel=~s/ \| $//; # remove trailing pipe as a result of the above loop
@@ -341,11 +238,12 @@ sub readMetadata{
 sub filterTrees{
   my($metadata,$settings)=@_;
 
+  logmsg "Filtering trees by time and/or by line list. If none were given, then no trees will be filtered by this process. If --new-isolates was given, we are also filtering by that!";
+
   # Index the list of new isolates if requested to 
   # filter by new isolates
   my %treeWithNewIsolate;
   if($$settings{'new-isolates'}){
-    logmsg "Filtering trees by new isolates, identified by $$settings{tempdir}/Clusters/*.new_isolates.tsv";
     my $newisolatesFile=(glob("$$settings{tempdir}/Clusters/*.new_isolates.tsv"))[0];
     open(my $fh,"<",$newisolatesFile) or die "ERROR: cannot read $newisolatesFile: $!";
     my $header=<$fh>; chomp($header);
@@ -361,7 +259,7 @@ sub filterTrees{
   }
 
   # Start the filtering process
-  logmsg "Filtering trees by time and/or by line list. If none were given, then no trees will be filtered by this process.";
+  my $numPassed=0;
   for my $tree(glob("$$settings{tempdir}/SNP_trees/*.newick")){
     # If the tree passes filters, then it gets moved here
     my $outtree="$$settings{outdir}/SNP_trees/".basename($tree);
@@ -419,7 +317,7 @@ sub filterTrees{
     # were supplied.
     if(@{$$settings{'line-list'}} > 0){
       
-      my $tree_has_linelist=0;
+      my $hasAnIsolateInLineList=0; # guilty-until-innocent
       for my $s(@sample){
         my $target_acc=$s->id;
         $target_acc=~s/^\'|\'$//g; # remove single quotes that might appear around taxa in the tree
@@ -429,21 +327,16 @@ sub filterTrees{
         # be the indication that it is in the line list.
         # TODO: I should probably just read the line list
         # directly.
-        if(
-             defined($$metadata{$target_acc}{Key}) # state ID
-          || defined($$metadata{$target_acc}{SourceCity}) 
-          || defined($$metadata{$target_acc}{SourceCounty})
-          || defined($$metadata{$target_acc}{PatientSex})
-          || defined($$metadata{$target_acc}{SeroType})
-          || defined($$metadata{$target_acc}{PatientAge})
-        ){
-          $tree_has_linelist=1;
-          last;
+        if($$metadata{$target_acc}{is_in_lineList}){
+          $hasAnIsolateInLineList++;
         }
       }
-      if($tree_has_linelist==0){
+
+      # If not a single isolate is in the line list, then this tree fails.
+      if(!$hasAnIsolateInLineList){
         $tree_passed_filters=0;
         push(@whyFail,"Tree does not have isolates in the line list");
+        next;
       }
     }
 
@@ -451,13 +344,15 @@ sub filterTrees{
 
     # If the tree passed all filters, then copy it over
     if($tree_passed_filters){
+      $numPassed++;
       cp($tree,$outtree) or die "ERROR copying $tree to $tree:\n  $!";
     } else {
+      unlink($outtree); # remove this file in case of previous results
       #logmsg "Tree failed the filters because ".join("\n",@whyFail);
     }
   }
 
-  logmsg "Done filtering";
+  logmsg "Done filtering. $numPassed trees passed.";
 
 }
 
@@ -470,20 +365,23 @@ sub makeReport{
   my $PDF="$$settings{outdir}/report.pdf";
 
   # Figure out a coloring scheme
-  my @colorPercentage=(0.1,0.3,0.5,0.7,0.9);
-  my @availableColor = variations_with_repetition(\@colorPercentage,3);
+  #  http://paletton.com/#uid=62X0Z0k7xFM3Ur75UujeqG4j0KZ
+  #my @colorPercentage=(0.1,0.3,0.5,0.7,0.9);
+  #my @availableColor = variations_with_repetition(\@colorPercentage,3);
   # Sort colors brightest to darkest instead of by RGB. Randomize colors
   # that have an equal score in the sort.
-  @availableColor=sort { sum(map{exp($_)} @$b) <=> sum(map{exp($_)} @$a) } shuffle(@availableColor);
+  #@availableColor=sort { sum(map{exp($_)} @$b) <=> sum(map{exp($_)} @$a) } shuffle(@availableColor);
   # Remove colors that are too bright
-  @availableColor=grep{!($$_[0]>0.7 && $$_[1] >0.7 && $$_[2] > 0.7)} @availableColor;
+  #@availableColor=grep{!($$_[0]>0.7 && $$_[1] >0.7 && $$_[2] > 0.7)} @availableColor;
   #die Dumper [map{join(", ",@$_)} @availableColor];
+
+  my @availableColor=colorScheme();
 
   # Figure out what categories the user wants to color by
   my $colorBy=new Config::Simple("config/colorBy.ini")->get_block("global");
   my @colorBy=keys(%$colorBy);
   my %colorCoding;#=(''=>[0,0,0]); # holds color coding combinations defined by the config file
-  $colorCoding{''} = pop(@availableColor); # when there is no color, choose black
+  $colorCoding{Missing} = shift(@availableColor); # when there is no color, choose black
 
   logmsg "Creating phylogeny images";
   mkdir "$$settings{outdir}/images";
@@ -543,8 +441,8 @@ sub makeReport{
   $p->setcolour("black");
   $p->setlinewidth(1);
   my $wholeLegendHeight=4*72;
-  my $wholeLegendY1=4*72;
-  $p->box(72*0.5,$wholeLegendY1,72*7.5,72*4+$wholeLegendHeight);
+  my $wholeLegendY1=2*72;
+  $p->box(72*0.5,$wholeLegendY1,72*7.5,$wholeLegendY1+$wholeLegendHeight);
 
   # The height/width of each color legend box is 2". For
   # keeping a margin, we will double the effective size
@@ -566,7 +464,7 @@ sub makeReport{
 
     $p->setfont("Times-Roman",int($ptPerColor));
     $p->setcolour("black");
-    $category||="No info";
+    $category||="Missing";
     #$p->text(72*1.0+$ptPerColor*2, $legendYMarker, $category."  ". join(", ",@$color)); # To the right of the box
     $p->text(72*1.0+$ptPerColor*2, $legendYMarker, $category); # To the right of the box
 
@@ -660,36 +558,6 @@ sub makeReport{
     logmsg "Converted $postscript to $PDF";
   }
 }
-# A hacky way to parse a variety of dates
-sub parseDate{
-  my($date,$settings)=@_;
-
-  return Time::Piece->strptime("12/31/1969","%m/%d/%Y") if($date=~/^\s*$/);
-
-  # Don't die just because Time::Piece craps out
-  my $timePiece;
-  eval{
-    if($date=~m|\d{1,2}/\d{1,2}/\d{2,4}|){
-      $timePiece=Time::Piece->strptime($date,"%m/%d/%Y");
-    } elsif($date=~m|\d{2,4}\-\d{1,2}\-\d{1,2}|){
-      $timePiece=Time::Piece->strptime($date,"%Y-%m-%d");
-    } else {
-
-      # IF MMYYDD formats don't work, try just the year
-      if($date=~/^\d{4}$/){
-        $timePiece=Time::Piece->strptime($date,"%Y");
-      } elsif($date=~/^(\d{4})/){ # just try first four digits of a number as a year
-        $timePiece=Time::Piece->strptime($1,"%Y");
-      }
-        
-    }
-    return $timePiece;
-  };
-  return $timePiece if(defined $timePiece);
-
-  logmsg "WARNING: I could not parse $date for dates. Using 12/31/1969.";
-  return Time::Piece->strptime("12/31/1969","%m/%d/%Y");
-}
 
 sub addMetadataToTree{
   my($treeObj,$metadata,$colorBy,$colorCoding,$availableColor,$settings)=@_;
@@ -709,7 +577,7 @@ sub addMetadataToTree{
     # TODO allow for more categories.
     for my $colorKey (keys(%$colorBy)){
       $$metadata{$target_acc}{$colorKey}//="";
-      if($$metadata{$target_acc}{$colorKey} =~/^NULL$|^\s*$|^missing$/){
+      if($$metadata{$target_acc}{$colorKey} =~/^NULL$|^\s*$|^missing$/i){
         next;
       }
 
@@ -721,7 +589,7 @@ sub addMetadataToTree{
     }
 
     if(!$color){
-      $color=$$colorCoding{''};
+      $color=$$colorCoding{Missing};
     }
 
     $node->add_tag_value("Rcolor",$$color[0]);
